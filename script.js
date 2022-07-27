7dd85359e09598130b769fc7709da7acf3ca9e3e
Rupali Mohapatra <102797868+Rupali-foocoding@users.noreply.github.com>
2022-07-27 20:16:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.slider-dot',
+    dotsClass: 'dots'
+  });
+  let hamburger = document.querySelector('.hamburger-button');
+  let times = document.querySelector('.times');
+  let mobileNav = document.querySelector('.mobile-nav');
+  let click = document.querySelector('.click');
+  let click1 = document.querySelector('.click-1');
+  let click2 = document.querySelector('.click-2');
+  let click3 = document.querySelector('.click-3');
+  hamburger.addEventListener('click', function () {
+    mobileNav.classList.add('open');
+  });
+  times.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+  click.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+  click1.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+  click2.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+  click3.addEventListener('click', function () {
+    mobileNav.classList.remove('open');
+  });
+});
